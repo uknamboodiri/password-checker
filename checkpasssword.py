910d4e64c85a9999d8b4a64100d9fed5571aa06d
Unnikrishnan <uknamboodiri@gmail.com>
2020-06-07 11:50:55 +0530

    refactored

--- checkpasssword.py
+++ checkpasssword.py
@@ -1,16 +1,49 @@
 import requests
 import hashlib
+import sys
 
-pwd = 'hello'
 
-SHA1_Hash = hashlib.sha1(pwd.encode('UTF-8')).hexdigest().upper()
-first_5_hash_chars, tail = SHA1_Hash[:5], SHA1_Hash[5:]
+# has the input
+# pull the 1st 5 chars
+# rest of the chars as tail
+# send the first 5 chars to API, get response
 
-response = requests.get(f'https://api.pwnedpasswords.com/range/{first_5_hash_chars}')
-hashes = (line.split(':') for line in response.text.splitlines())
+def hash_the_given_password(pwd):
+    return hashlib.sha1(pwd.encode('UTF-8')).hexdigest().upper()
 
-for h, count in hashes:
-    # print(h, count)
-    if tail == h:
-        print(count)
-        break
+
+def get_api_response(first_5_hash_chars):
+    response = requests.get(f'https://api.pwnedpasswords.com/range/{first_5_hash_chars}')
+
+    if response.status_code != 200:
+        raise RuntimeError(f'Invalid request, kindly check your password {first_5_hash_chars}')
+    else:
+        return response
+
+
+def check_if_used(pwd):
+    hashed_pwd = hash_the_given_password(pwd)
+    first_5_hash_chars, tail = hashed_pwd[:5], hashed_pwd[5:]
+
+    response = get_api_response(first_5_hash_chars)
+    hashes = (line.split(':') for line in response.text.splitlines())
+
+    for h, count in hashes:
+        if tail == h:
+            return count
+    return 0
+
+
+def main(args):
+    for pwd in args:
+        count = check_if_used(pwd)
+        if count:
+            print(f'"{pwd}" discovered {count} times, you may NOT want to use this anywhere!!')
+        else:
+            print(f'{count} instances found, carry on! ')
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
